a63808d5c29459d180a788312127b4d7560f0c54
Arnel Arellano <arndim.z01@gmail.com>
2023-03-14 10:30:57 +0800

    test vm machine

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -44,11 +44,12 @@ static void my_application_activate(GApplication* application) {
     //gtk_header_bar_set_show_close_button(header_bar, TRUE);
     //gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    //gtk_window_set_title(window, "syncappkiosk");
+    gtk_window_set_title(window, "syncappkiosk");
   }
 
   //gtk_window_set_default_size(window, 1280, 720);
-  gtk_widget_show(GTK_WIDGET(window));
+  gtk_window_fullscreen(GTK_WIDGET(window));
+  //gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
   fl_dart_project_set_dart_entrypoint_arguments(project, self->dart_entrypoint_arguments);
