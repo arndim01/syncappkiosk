d3bf4bbdd37a3b5c5987e9425910e63203dce8b3
Arnel Arellano <Arnel.Arellano@nafinc.com>
2023-03-28 18:11:07 +0800

    duration 30 splash page

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           return null;
         },
         routes: {
-          '/': (context) => const SplashPage(duration: 1, goToPage: '/started'),
+          '/': (context) => const SplashPage(duration: 30, goToPage: '/started'),
           '/kioskmain': (context) => const KioskMainPage(),
           '/cashinservice': (context) => const CashInServicePage(),
           '/gcashform': (context) => const LoaderOverlay(
